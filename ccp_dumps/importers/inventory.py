be88f8402f2195dbba8ff9a53647924d1807c944
Greg Taylor <gtaylor@duointeractive.com>
2009-09-24 17:32:45 +0000

    Applying Dylan's importer patch to fix import order.
    git-svn-id: https://django-eve.googlecode.com/svn/trunk@106 0d93ab44-f725-11dd-b507-8b4ecd5210be

--- ccp_dumps/importers/inventory.py
+++ ccp_dumps/importers/inventory.py
@@ -166,6 +166,101 @@ def do_import_metatypes(conn):
                                                                       meta_group=meta_group)
         imp_obj.save()
     c.close()
+
+
+
+def do_import_eve_units(conn):
+    """
+    Handle the import.
+    """
+    c = conn.cursor()
+    
+    for row in c.execute('select * from eveUnits'):
+        imp_obj, created = EVEUnit.objects.get_or_create(id=row['unitid'])
+        imp_obj.name = row['unitname']
+        imp_obj.display_name = row['displayname']
+        imp_obj.description = row['description']
+
+        imp_obj.save()
+
+    # Clean up.
+    c.close()
+
+def do_import_attribute_categories(conn):
+    """
+    Handle the import.
+    """
+
+    c = conn.cursor()
+
+    for row in c.execute('select * from dgmattributecategories'):
+        imp_obj, created = EVEInventoryAttributeCategory.objects.get_or_create(id=row['categoryid'])
+        imp_obj.name = row['categoryname']
+        imp_obj.description = row['categorydescription']
+
+        imp_obj.save()
+
+    # Clean up.
+    c.close()
+
+def do_import_attribute_types(conn):
+    """
+    Handle the import.
+    """
+
+    c = conn.cursor()
+
+    for row in c.execute('select * from dgmattributetypes'):
+        imp_obj, created = EVEInventoryAttributeType.objects.get_or_create(id=row['attributeid'])
+        imp_obj.name = row['attributename']
+        imp_obj.description = row['description']
+        imp_obj.default_value = row['defaultvalue']
+        imp_obj.is_published = constants.parse_int_bool(row['published'])
+        imp_obj.display_name = row['displayname']
+        imp_obj.is_stackable = constants.parse_int_bool(row['stackable'])
+        imp_obj.high_is_good = constants.parse_int_bool(row['highisgood'])
+
+        category_id = row['categoryid']
+        if category_id:
+            imp_obj.category = EVEInventoryAttributeCategory.objects.get(id=category_id)
+
+        unit_id = row['unitid']
+        if unit_id:
+            imp_obj.unit = EVEUnit.objects.get(id=unit_id)
+
+        graphic_id = row['graphicID']
+        if graphic_id:
+            imp_obj.graphic = EVEGraphic.objects.get(id=graphic_id)
+
+        imp_obj.save()
+            
+    # Clean up.
+    c.close()
+
+def do_import_inventory_type_attributes(conn):
+    """
+    Handle the import.
+    """
+
+    c = conn.cursor()
+
+    for row in c.execute('select * from dgmtypeattributes'):    
+        inventory_type = EVEInventoryType.objects.get(id=row['typeid'])
+        attribute = EVEInventoryAttributeType.objects.get(id=row['attributeid'])
+        imp_obj, created = EVEInventoryTypeAttributes.objects.get_or_create(inventory_type=inventory_type,
+                                                                            attribute=attribute)
+
+        if row['valueint']:
+            imp_obj.value_int = row['valueint']
+
+        if row['valuefloat']:
+            imp_obj.value_float = row['valuefloat']
+
+        imp_obj.save()
+
+    # Clean up.
+    c.close()
+
     
 def do_import_effects(conn):
     """
@@ -420,6 +515,10 @@ def do_import():
     do_import_groups(conn)
     do_import_market_groups(conn)
     do_import_invtypes(conn)
+    do_import_eve_units(conn)
+    do_import_attribute_categories(conn)
+    do_import_attribute_types(conn)
+    do_import_inventory_type_attributes(conn)
     do_import_blueprint_types(conn)
     do_import_activity_materials(conn)
     do_import_eve_names(conn)
--- ccp_dumps/run_importers.py
+++ ccp_dumps/run_importers.py
@@ -8,13 +8,13 @@ sys.path.insert(0, project_root)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 # Import the importer modules.
-from importers import chr, graphics, inventory, station, attributesandeffects, map, npc
+from importers import chr, graphics, inventory, station,  map, npc
 
 print "NOTE: This is going to take anywhere from 15-30 minutes depending on the speed of your computer and DB server. You may abort it at any time and re-start it later via CTRL-D or CTRL-C, depending on your platform."
 print "Importing from the CCP dump..."
 
 # Carry out the imports in order.
-for mod in [graphics, chr, station, inventory, attributesandeffects, map, npc]:
+for mod in [graphics, chr, station, inventory,  map, npc]:
     print "  - %s" % mod.__name__
     mod.do_import()
     
