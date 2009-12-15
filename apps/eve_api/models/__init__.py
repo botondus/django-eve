1bb309d4dc9097371f210f70476ab9f36d4df25f
Greg Taylor <gtaylor@duointeractive.com>
2009-12-15 19:58:12 +0000

    Removing eve_api, it's got its own project now.
    git-svn-id: https://django-eve.googlecode.com/svn/trunk@141 0d93ab44-f725-11dd-b507-8b4ecd5210be

--- apps/eve_api/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-By importing all of these sub-modules, the models package is transparently
-accessible by the rest of the project. This makes it act just as if it were
-one monolithic models.py.
-"""
-from api_player import *
